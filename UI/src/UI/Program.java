0095b8824ea0715c5e6fd1abc3c1969857e33f85
NivGorsky <gorsky7411@gmail.com>
2022-04-11 14:53:00 +0300

    Chaned to take the input not from tests

--- UI/src/UI/Program.java
+++ UI/src/UI/Program.java
@@ -6,15 +6,6 @@ public class Program {
 
     public static void main(String[] args)
     {
-        try {
-            File f = new File("test2.txt");
-            InputStream inputStream = new FileInputStream(f);
-            System.setIn(inputStream);
-        }
-        catch (Exception e){
-            System.out.println(e.getMessage());
-        }
-
         ConsoleUI systemUI = new ConsoleUI();
         systemUI.runMainMenu();
     }
