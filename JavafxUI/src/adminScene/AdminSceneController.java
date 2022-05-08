398a387c10c580fa004dae7f7b093d7733623d1e
Ofek Cohen <ofekcohen1996@gmail.com>
2022-05-08 12:53:56 +0300

    12:53

---
+++ JavafxUI/src/adminScene/AdminSceneController.java
@@ -0,0 +1,40 @@
+
+package adminScene;
+
+import generalScenes.HeaderPaneController;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+public class AdminSceneController {
+
+        @FXML private AnchorPane headerPane;
+        @FXML private HeaderPaneController headerPaneController;
+        @FXML private Button increaseYazButton;
+        @FXML private Button loadFileButton;
+        @FXML private Button loansButton;
+        @FXML private Button customersInformationButton;
+
+        @FXML public void initialize()
+        {
+            
+        }
+
+        @FXML void customersInformationClicked(ActionEvent event) {
+
+        }
+
+        @FXML void increaseYazClicked(ActionEvent event) {
+
+        }
+
+        @FXML void loadFileClicked(ActionEvent event) {
+
+        }
+
+        @FXML void loansClicked(ActionEvent event) {
+
+        }
+
+    }
+
---
+++ JavafxUI/src/generalScenes/HeaderPaneController.java
@@ -0,0 +1,18 @@
+package generalScenes;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HeaderPaneController {
+
+    @FXML
+    private Label viewByLable;
+
+    @FXML
+    private Label filePathLabel;
+
+    @FXML
+    private Label currentYazLabel;
+
+}
+
