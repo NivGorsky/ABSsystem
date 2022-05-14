fc18adf0c4832b84acf15daaf95b418cfcaf8b66
NivGorsky <gorsky7411@gmail.com>
2022-05-14 12:07:29 +0300

    Created charge and withdraw methods
    Need to understand where does the relevant fields for each component should be.
    Need to understand how to link the table view to the data.

--- JavafxUI/src/customer/CustomerController.java
+++ JavafxUI/src/customer/CustomerController.java
@@ -1,9 +1,12 @@
 package customer;
+import DTO.CustomerDTO;
 import Engine.ABSsystem;
+import Engine.Customer;
 import Engine.MainSystem;
 import customer.information.InformationController;
 import customer.payment.PaymentController;
 import customer.scramble.ScrambleController;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.ScrollPane;
@@ -12,10 +15,29 @@ import javafx.scene.control.ScrollPane;
 public class CustomerController {
 
     private StringProperty customerNameProperty;
+    private CustomerDTO customerDTO;
     private MainSystem model;
+
+    private boolean areControllersInitialized(InformationController customerController, ScrambleController scrambleController, PaymentController paymentController){
+
+        return customerController != null && scrambleController != null && paymentController != null;
+    }
+
+    public void setModel(MainSystem model){
+        this.model = model;
+        this.informationController.setModel(model);
+    }
+
+    public void updateCustomerDTO(){
+        customerDTO = model.getCustomerDTO(customerNameProperty.getValue());
+    }
 
     public StringProperty getCustomerNameProperty(){
         return customerNameProperty;
+    }
+
+    public CustomerDTO getCustomerDTO(){
+        return customerDTO;
     }
 
     @FXML private ScrollPane information;
@@ -24,7 +46,6 @@ public class CustomerController {
     @FXML private ScrambleController scrambleController;
     @FXML private ScrollPane payment;
     @FXML private PaymentController paymentController;
-
     @FXML
     public void initialize(){
 
@@ -34,23 +55,6 @@ public class CustomerController {
             paymentController.setParentController(this);
         }
 
-
-
-
-
-
-
-
-
-    }
-
-    private boolean areControllersInitialized(InformationController customerController, ScrambleController scrambleController, PaymentController paymentController){
-
-        return customerController != null && scrambleController != null && paymentController != null;
-    }
-    public void setModel(MainSystem model){
-        this.model = model;
-        this.informationController.setModel(model);
     }
 
 
--- JavafxUI/src/customer/information/InformationController.java
+++ JavafxUI/src/customer/information/InformationController.java
@@ -1,4 +1,5 @@
 package customer.information;
+import Engine.SystemService;
 import com.sun.javafx.collections.ObservableListWrapper;
 import customer.CustomerController;
 import customer.information.accountTransactions.accountTransactionsController;
@@ -27,39 +28,64 @@ public class InformationController {
 
     private CustomerController parentController;
     private MainSystem model;
-    private SimpleListProperty<AccountMovementDTO> accountMovements;
-    private SimpleStringProperty customerName;
+    private SimpleListProperty<AccountMovementDTO> accountMovements; //need to check where this field will be, understand first how the table view works
+
+    public InformationController () {
+        accountMovements = new SimpleListProperty<>();
+    }
+
+    private void updateAccountMovements(){
+        parentController.updateCustomerDTO();
+        CustomerDTO customerDTO = parentController.getCustomerDTO();
+        List<AccountMovementDTO> movementsFromEngine = customerDTO.getAccountMovements();
+        ObservableListWrapper<AccountMovementDTO> observableMovements = new ObservableListWrapper<>(movementsFromEngine);
+        this.accountMovements.setValue(observableMovements); //need to check how to implement this in a table view
+    }
+
+    public void setParentController(CustomerController parentController) {
+        this.parentController = parentController;
+    }
+
+    public void setModel(MainSystem  model){
+        this.model = model;
+    }
 
     @FXML private ScrollPane accountTransactions;
     @FXML private accountTransactionsController accountTransactionsController;
-
-    @FXML
-    private Button chargeButton;
-
-    @FXML
-    private TextField amountTextField;
-
-    @FXML
-    private Button withdrawButton;
+    @FXML private Button chargeButton;
+    @FXML private TextField amountTextField;
+    @FXML private Button withdrawButton;
 
     @FXML
     void chargeButtonClicked(ActionEvent event) {
         String customerName = parentController.getCustomerNameProperty().getValue();
         double amount = Double.parseDouble(amountTextField.getText());
 
-        //make the actual deposit
-        model.depositMoney(customerName, amount);
+        try {
+            //make the actual deposit
+            model.depositMoney(customerName, amount);
+            updateAccountMovements();
+        }
 
-        //update the movements table
-        CustomerDTO customerDTO = model.getCustomerDTO(customerName);
-        List<AccountMovementDTO> movementsFromEngine = customerDTO.getAccountMovements();
-        ObservableListWrapper<AccountMovementDTO> observableMovements = new ObservableListWrapper<>(movementsFromEngine);
-        this.accountMovements.setValue(observableMovements);
+        catch (Exception e){
+            //present an error window to the user
+        }
     }
 
     @FXML
     void withdrawButtonClicked(ActionEvent event) {
+        String customerName = parentController.getCustomerNameProperty().getValue();
+        double amount = Double.parseDouble(amountTextField.getText());
 
+        //make the actual withdraw
+        try {
+            model.withdrawMoney(customerName, amount);
+            updateAccountMovements();
+        }
+
+        catch (Exception e){
+            //present an error window to the user
+        }
     }
 
     @FXML
@@ -67,20 +93,7 @@ public class InformationController {
         if (accountTransactionsController != null){
             accountTransactionsController.setParentController(this);
         }
-        
-        //update customer name
+
         updateAccountMovements();
     }
-
-    public InformationController () {
-        accountMovements = new SimpleListProperty<>();
-    }
-
-    private void updateAccountMovements(){
-        CustomerDTO customerDTO = model.getCustomerDTO(customerName.get());
-        List<AccountMovementDTO> movementsFromEngine = customerDTO.getAccountMovements();
-        ObservableListWrapper<AccountMovementDTO> observableMovements = new ObservableListWrapper<>(movementsFromEngine);
-        this.accountMovements.setValue(observableMovements);
-    }
-
 }
