319b92d128a280edab3250477eb888e0732b5fc5
Ofek Cohen <ofekcohen1996@gmail.com>
2022-06-09 15:46:21 +0300

    loginScene

---
+++ JavafxUI/src/loginScene/LoginController.java
@@ -0,0 +1,17 @@
+package loginScene;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML private ScrollPane Login;
+    @FXML private Label loginLabel;
+    @FXML private Label nameLabel;
+    @FXML private TextField nameTextField;
+    @FXML private Button OKButton;
+
+}
