912b88da181215942a3d9543f73cfcd6e6532e15
NivGorsky <gorsky7411@gmail.com>
2022-07-11 15:53:27 +0300

    update
    added the feature of getting the loans by customer name, and for each type of loans - lender loans and borrower loans..
    
    need to check with offek that the file is being uploaded succuessfuly.

--- AbsServer/src/ShowLoansInfoServlet.java
+++ AbsServer/src/ShowLoansInfoServlet.java
@@ -1,3 +1,4 @@
+import DTO.CustomerDTO;
 import DTO.LoanDTO;
 import Engine.MainSystem;
 import jakarta.servlet.annotation.WebServlet;
@@ -17,8 +18,28 @@ public class ShowLoansInfoServlet extends HttpServlet
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("application/json");
         MainSystem mainSystem = ServletUtils.getAbsSystem(getServletContext());
+        ArrayList<LoanDTO> loansInfo = new ArrayList<>();
 
-        ArrayList<LoanDTO> loansInfo = mainSystem.showLoansInfo();
+        if(!request.getParameterMap().isEmpty()){
+            String customerName = request.getParameter("customer-name");
+
+            switch (request.getParameter("loan-type")){
+                case "lender":
+                    loansInfo = mainSystem.getLoansByCustomerNameAsLender(customerName);
+
+                    break;
+
+                case "borrower":
+                    loansInfo = mainSystem.getLoansByCustomerNameAsBorrower(customerName);
+
+                    break;
+            }
+        }
+
+        else{
+            loansInfo = mainSystem.showLoansInfo();
+        }
+
         String loansInfoAsJsonString = ServletUtils.GSON.toJson(loansInfo);
         response.getWriter().print(loansInfoAsJsonString);
         response.getWriter().flush();
--- JavafxUI/src/loansTable/LoansTableComponentController.java
+++ JavafxUI/src/loansTable/LoansTableComponentController.java
@@ -215,14 +215,80 @@ public class LoansTableComponentController implements ParentController {
 //
     public void loadSpecificCustomerLoansAsLender(String customerName){
         createTableLoanColumns();
-        ArrayList<LoanDTO> loans = parentController.getModel().getLoansByCustomerNameAsLender(customerName);
-        putLoansInTable(loans);
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(Configurations.BASE_URL + "/showLoansInfo").newBuilder();
+        urlBuilder.addQueryParameter("customer-name", customerName);
+        urlBuilder.addQueryParameter("loans-type", "lender");
+        String finalUrl = urlBuilder.build().toString();
+        Request request = new Request.Builder().url(finalUrl).get().build();
+        Call call = Configurations.HTTP_CLIENT.newCall(request);
+
+        Callback showLoansInfoCallBack = new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                Platform.runLater(() -> {
+                    parentController.createExceptionDialog(e);
+                });
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                if(response.isSuccessful()){
+                    String rawBody = response.body().string();
+                    Type arrayListLoanDtoType = new TypeToken<ArrayList<LoanDTO>>(){}.getType();
+                    ArrayList<LoanDTO> loans = Configurations.GSON.fromJson(rawBody, arrayListLoanDtoType);
+
+                    Platform.runLater(() -> {
+                        putLoansInTable(loans);
+                    });
+                }
+
+                else{
+                    parentController.createExceptionDialog(new Exception(Integer.toString(response.code())));
+                }
+            }
+        };
+
+        call.enqueue(showLoansInfoCallBack);
+
     }
 
     public void loadSpecificCustomerLoansAsBorrower(String customerName){
         createTableLoanColumns();
-        ArrayList<LoanDTO> loans = parentController.getModel().getLoansByCustomerNameAsBorrower(customerName);
-        putLoansInTable(loans);
+        HttpUrl.Builder urlBuilder = HttpUrl.parse(Configurations.BASE_URL + "/showLoansInfo").newBuilder();
+        urlBuilder.addQueryParameter("customer-name", customerName);
+        urlBuilder.addQueryParameter("loans-type", "borrower");
+
+        String finalUrl = urlBuilder.build().toString();
+        Request request = new Request.Builder().url(finalUrl).get().build();
+        Call call = Configurations.HTTP_CLIENT.newCall(request);
+
+        Callback showLoansInfoCallBack = new Callback() {
+            @Override
+            public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                Platform.runLater(() -> {
+                    parentController.createExceptionDialog(e);
+                });
+            }
+
+            @Override
+            public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                if(response.isSuccessful()){
+                    String rawBody = response.body().string();
+                    Type arrayListLoanDtoType = new TypeToken<ArrayList<LoanDTO>>(){}.getType();
+                    ArrayList<LoanDTO> loans = Configurations.GSON.fromJson(rawBody, arrayListLoanDtoType);
+
+                    Platform.runLater(() -> {
+                        putLoansInTable(loans);
+                    });
+                }
+
+                else{
+                    parentController.createExceptionDialog(new Exception(Integer.toString(response.code())));
+                }
+            }
+        };
+
+        call.enqueue(showLoansInfoCallBack);
     }
 
     private void putLoansInTable( ArrayList<LoanDTO> loans){
