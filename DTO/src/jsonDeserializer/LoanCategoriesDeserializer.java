267c9e4229d5eddf086ea00a7178433b98803771
Ofek Cohen <ofekcohen1996@gmail.com>
2022-07-15 09:48:22 +0300

    LoanCategoriesDTO deserializer

--- DTO/src/jsonDeserializer/LoanCategoriesDeserializer.java
+++ DTO/src/jsonDeserializer/LoanCategoriesDeserializer.java
@@ -1,19 +1,30 @@
 package jsonDeserializer;
 
+import DTO.LoanCategoriesDTO;
 import DTO.LoanDTO;
 import com.google.gson.*;
 import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
-public class LoanCategoriesDeserializer implements JsonDeserializer<LoanDTO> {
+public class LoanCategoriesDeserializer implements JsonDeserializer<LoanCategoriesDTO> {
 
     Gson gson = new Gson();
 
     @Override
-    public LoanDTO deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+    public LoanCategoriesDTO deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+        JsonObject jsonObject = jsonElement.getAsJsonObject();
+
+
         Type loanDtoType = new TypeToken<ArrayList<String>>(){}.getType();
-        JsonElement categoriesAsJson = 
+        JsonArray categoriesAsJsonObject =  jsonObject.getAsJsonArray("loanCategories");
+        ArrayList<String> loanCategories = gson.fromJson(categoriesAsJsonObject, loanDtoType);
+
+        LoanCategoriesDTO loanCategoriesDTO = new LoanCategoriesDTO(loanCategories);
+
+        return loanCategoriesDTO;
     }
 }
