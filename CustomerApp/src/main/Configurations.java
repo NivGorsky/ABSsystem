021521dab9bd3fcf0a216cd54dcd45d27a217932
NivGorsky <gorsky7411@gmail.com>
2022-07-16 11:59:26 +0300

    Added the update notifications feature

--- CustomerApp/src/main/Configurations.java
+++ CustomerApp/src/main/Configurations.java
@@ -1,9 +1,11 @@
 package main;
 import DTO.LoanDTO;
+import DTO.NotificationsDTO;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import jsonDeserializer.NotificationsDTODeserializer;
 import okhttp3.OkHttpClient;
 import jsonDeserializer.LoanDtoDeserializer;
 
@@ -11,6 +13,10 @@ public class Configurations {
     public final static String BASE_URL = "http://localhost:8080/AbsServer";
     public final static OkHttpClient HTTP_CLIENT = new OkHttpClient();
     public final static String LINE_SEPARATOR = System.getProperty("line.separator");
-    public final static Gson GSON = new GsonBuilder().registerTypeAdapter(LoanDTO.class, new LoanDtoDeserializer()).create();
+    public final static Gson GSON = new GsonBuilder()
+            .registerTypeAdapter(LoanDTO.class, new LoanDtoDeserializer())
+            .registerTypeAdapter(NotificationsDTO.class, new NotificationsDTODeserializer())
+            .create();
+    
 //public final static Gson GSON = new Gson();
 }
---
+++ DTO/src/jsonDeserializer/NotificationsDTODeserializer.java
@@ -0,0 +1,32 @@
+package jsonDeserializer;
+import DTO.LoanDTO;
+import DTO.NotificationsDTO;
+import com.google.gson.*;
+import com.google.gson.reflect.TypeToken;
+import javafx.beans.property.SimpleStringProperty;
+import javax.security.auth.login.Configuration;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.SortedMap;
+
+public class NotificationsDTODeserializer implements JsonDeserializer<NotificationsDTO> {
+
+    private Gson gson = new Gson();
+
+    @Override
+    public NotificationsDTO deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        //extract raw data
+        JsonObject jsonObject = json.getAsJsonObject();
+
+        //build the actual object
+        Type notificationsDTOType = new TypeToken<List<NotificationsDTO.NotificationDTO>>(){}.getType();
+        JsonObject notificationsAsJsonObject = jsonObject.getAsJsonObject("notifications");
+        List<NotificationsDTO.NotificationDTO> notificationDTOSFromJson = gson.fromJson(notificationsAsJsonObject, notificationsDTOType);
+        NotificationsDTO newNotificationsDTO = new NotificationsDTO();
+        newNotificationsDTO.notifications = notificationDTOSFromJson;
+
+        return newNotificationsDTO;
+    }
+}
