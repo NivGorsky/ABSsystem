f27dbdfa1ba1d6613c4c2a0e920ddffdb3182af5
Ofek Cohen <ofekcohen1996@gmail.com>
2022-07-18 17:16:17 +0300

    Create RewindDTO.java

---
+++ DTO/src/DTO/RewindDTO.java
@@ -0,0 +1,10 @@
+package DTO;
+
+import java.util.ArrayList;
+
+public class RewindDTO {
+
+    private int currentYaz;
+    private ArrayList<CustomerDTO> customers;
+    private ArrayList<LoanDTO> loans;
+}
