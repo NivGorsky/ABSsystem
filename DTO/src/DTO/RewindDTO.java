aa596201a5bc9f93ef00dfd0643e348b0f33ad3e
Ofek Cohen <ofekcohen1996@gmail.com>
2022-07-18 17:42:12 +0300

    kf

--- DTO/src/DTO/RewindDTO.java
+++ DTO/src/DTO/RewindDTO.java
@@ -7,4 +7,22 @@ public class RewindDTO {
     private int currentYaz;
     private ArrayList<CustomerDTO> customers;
     private ArrayList<LoanDTO> loans;
+
+    public RewindDTO(int yaz, ArrayList<CustomerDTO> customers, ArrayList<LoanDTO> loans) {
+        this.currentYaz = yaz;
+        this.customers = customers;
+        this.loans = loans;
+    }
+
+    public int getCurrentYaz() {
+        return currentYaz;
+    }
+
+    public ArrayList<CustomerDTO> getCustomers() {
+        return customers;
+    }
+
+    public ArrayList<LoanDTO> getLoans() {
+        return loans;
+    }
 }
